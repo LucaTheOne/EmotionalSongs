29403c52a5b170dc7b3a6ee128fcf6421fd2bc70
LucaTheOne <big.luca97@outlook.it>
2022-10-11 11:55:43 +0200

    k

--- src/main/java/emotionalsongs/Managers/VoteManager.java
+++ src/main/java/emotionalsongs/Managers/VoteManager.java
@@ -1,18 +1,12 @@
 
 package emotionalsongs.Managers;
 
-import emotionalsongs.BasicsStructure.*;
-
 /**
  *
  * @author big
  */
 public class VoteManager {
     
-    int voteToAddToAmazement;
     
-    public void setEmotionVote(Emozioni emotion, int i) {
-        
-    }
     
 }
