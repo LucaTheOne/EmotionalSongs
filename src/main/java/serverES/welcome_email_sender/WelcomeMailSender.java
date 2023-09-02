3abc106ab55389c2087f39b8b61e68cb1d2c8630
LucaTheOne <big.luca97@outlook.it>
2023-09-02 11:39:09 +0200

    k

--- src/main/java/serverES/welcome_email_sender/WelcomeMailSender.java
+++ src/main/java/serverES/welcome_email_sender/WelcomeMailSender.java
@@ -75,7 +75,7 @@ public class WelcomeMailSender {
             // Send message
             Transport.send(message);
         } catch (MessagingException mex) {
-            mex.printStackTrace();
+            mex.getMessage();
         }
     }
     /*
